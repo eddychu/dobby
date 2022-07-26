dd2a3322caa6357cb50cff360ece74447cdff007
eddychu <hey.yadong@gmail.com>
2022-07-26 23:12:18 +0800

    add camera view projection

--- main.cpp
+++ main.cpp
@@ -2,13 +2,21 @@
 #include <SDL2/SDL.h>
 #include "core/model.h"
 #include "core/rasterizer.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 int main()
 {
+    int width = 800;
+    int height = 600;
     // Model model("data/models/Box/glTF-Embedded/Box.gltf");
     Model model("data/models/DamagedHelmet/glTF-Embedded/DamagedHelmet.gltf");
     Texture texture("DamagedHelmet/glTF/Default_albedo.jpg");
-    int width = 800;
-    int height = 600;
+
+    auto projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
+    auto view = glm::lookAt(vec3(0, 0, 5), vec3(0, 0, 0), vec3(0, 1, 0));
+
+    auto mvp = projection * view;
+
     SDL_Init(SDL_INIT_VIDEO);
     auto window = SDL_CreateWindow("Hello World",
                                    SDL_WINDOWPOS_UNDEFINED,
@@ -42,6 +50,13 @@ int main()
                 const auto &p2 = vertices[indices[i + 1]].position;
                 const auto &p3 = vertices[indices[i + 2]].position;
 
+                auto clip_p1 = mvp * vec4(p1, 1.0);
+                auto clip_p2 = mvp * vec4(p2, 1.0);
+                auto clip_p3 = mvp * vec4(p3, 1.0);
+                clip_p1 /= clip_p1.w;
+                clip_p2 /= clip_p2.w;
+                clip_p3 /= clip_p3.w;
+
                 const auto &t1 = vertices[indices[i]].tex_coords;
                 const auto &t2 = vertices[indices[i + 1]].tex_coords;
                 const auto &t3 = vertices[indices[i + 2]].tex_coords;
@@ -50,7 +65,7 @@ int main()
                 auto c2 = texture.sample(t2);
                 auto c3 = texture.sample(t3);
 
-                TriangleInfo info{p1, p2, p3, c1, c2, c3};
+                TriangleInfo info{clip_p1, clip_p2, clip_p3, c1, c2, c3};
 
                 Rasterizer::rasterize(info, &frame_buffer, Rasterizer::Mode::TRIANGLE);
             }
--- src/core/rasterizer.h
+++ src/core/rasterizer.h
@@ -5,12 +5,12 @@ class FrameBuffer;
 
 struct TriangleInfo
 {
-    const vec3 &v1;
-    const vec3 &v2;
-    const vec3 &v3;
-    const vec4 &c1;
-    const vec4 &c2;
-    const vec4 &c3;
+    const vec4 v1;
+    const vec4 v2;
+    const vec4 v3;
+    const vec4 c1;
+    const vec4 c2;
+    const vec4 c3;
 };
 
 class Rasterizer
